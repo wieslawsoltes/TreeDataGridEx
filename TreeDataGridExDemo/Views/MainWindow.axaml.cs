8c38d6e4792cb92c3175cdc4fdb0902040aa2bc5
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2023-08-12 12:14:14 +0200

    Update MainWindow.axaml.cs

--- TreeDataGridExDemo/Views/MainWindow.axaml.cs
+++ TreeDataGridExDemo/Views/MainWindow.axaml.cs
@@ -1,5 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Rendering;
+using TreeDataGridExDemo.Models;
 
 namespace TreeDataGridExDemo.Views;
 
@@ -10,5 +13,35 @@ public partial class MainWindow : Window
         InitializeComponent();
 
         // RendererDiagnostics.DebugOverlays = RendererDebugOverlays.Fps |RendererDebugOverlays.LayoutTimeGraph | RendererDebugOverlays.RenderTimeGraph;
+        
+        TreeDataGridExDragDrop.Loaded += TreeDataGridExDragDropOnLoaded;
     }
+
+    private void TreeDataGridExDragDropOnLoaded(object? sender, RoutedEventArgs e)
+    {
+        if (TreeDataGridExDragDrop.TreeDataGrid is { } treeDataGrid)
+        {
+            treeDataGrid.AutoDragDropRows = true;
+            treeDataGrid.RowDragStarted += DragDrop_RowDragStarted;
+            treeDataGrid.RowDragOver += DragDrop_RowDragOver;
+        }
+    }
+
+    private void DragDrop_RowDragStarted(object? sender, TreeDataGridRowDragStartedEventArgs e)
+    {
+        foreach (DragDropItem i in e.Models)
+        {
+            if (!i.AllowDrag)
+                e.AllowedEffects = DragDropEffects.None;
+        }
+    }
+
+    private void DragDrop_RowDragOver(object? sender, TreeDataGridRowDragEventArgs e)
+    {
+        if (e.Position == TreeDataGridRowDropPosition.Inside &&
+            e.TargetRow.Model is DragDropItem i &&
+            !i.AllowDrop)
+            e.Inner.DragEffects = DragDropEffects.None;
+    }
+
 }
