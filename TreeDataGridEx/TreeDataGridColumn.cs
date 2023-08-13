c0711933f91976ce91bb83e691bc3cd22dca6de7
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2023-08-13 12:40:41 +0200

    Update TreeDataGridColumn.cs

--- TreeDataGridEx/TreeDataGridColumn.cs
+++ TreeDataGridEx/TreeDataGridColumn.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
 using Avalonia;
+using Avalonia.Metadata;
 
 namespace TreeDataGridEx;
 
 public abstract class TreeDataGridColumn : AvaloniaObject
 {
+    [InheritDataTypeFromItems(nameof(TreeDataGridEx.ItemsSource), AncestorType = typeof(TreeDataGridEx))]
+    [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] 
+    public Type? DataType { get; set; }
 }
