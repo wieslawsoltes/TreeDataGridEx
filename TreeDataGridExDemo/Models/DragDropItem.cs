8eec4473f0088cd4a464c919f7db205118f4fbda
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-12-17 09:57:51 +0100

    Update

--- TreeDataGridExDemo/Models/DragDropItem.cs
+++ TreeDataGridExDemo/Models/DragDropItem.cs
@@ -7,12 +7,13 @@ namespace TreeDataGridExDemo.Models
 {
     public class DragDropItem : ReactiveObject
     {
-        private static Random _random = new Random(0);
+        private static readonly Random s_random = new Random(0);
         private ObservableCollection<DragDropItem>? _children;
         private bool _allowDrag = true;
         private bool _allowDrop = true;
 
         public DragDropItem(string name) => Name = name;
+
         public string Name { get; }
 
         public bool AllowDrag
@@ -32,7 +33,7 @@ namespace TreeDataGridExDemo.Models
         public static ObservableCollection<DragDropItem> CreateRandomItems()
         {
             var names = new Bogus.DataSets.Name();
-            var count = _random.Next(10);
+            var count = s_random.Next(10);
             return new ObservableCollection<DragDropItem>(Enumerable.Range(0, count)
                 .Select(x => new DragDropItem(names.FullName())));
         }
